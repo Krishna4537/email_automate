ea70448a684ccb19966557f10081437cc5e754f8
Krishna <85607892+Krishna4537@users.noreply.github.com>
2023-11-19 04:19:38 +0100

    updated script

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,23 +1,21 @@
 pipeline {
     agent any
 
-    parameters {
-        string(name: 'RECIPIENTS', defaultValue: 'john.smith@test.com,rodger.more@test.com', description: 'Email Recipients')
-        string(name: 'SENDER', defaultValue: 'no-reply-sre@test.com', description: 'Email Sender')
-        string(name: 'MESSAGE', defaultValue: 'Service XYZ has planned maintenance on Saturday from 14:00 till 17:00 CET', description: 'Email Message')
-    }
-
-    triggers {
-        cron('0 8 * * 6') // Every Saturday at 8 AM
-    }
-
     stages {
-        stage('Send Email') {
+        stage('Clone Repository') {
             steps {
-                script {
-                    sh "python/email_automate/email.py/email.py '${params.RECIPIENTS}' '${params.SENDER}' '${params.MESSAGE}'"
-                }
+                git 'https://github.com/your-username/your-repository.git'
+            }
+        }
+
+        stage('Send Maintenance Notification Email') {
+            steps {
+                sh 'python send_maintenance_email.py '${params.RECIPIENTS}' '${params.SENDER}' '${params.MESSAGE}''
             }
         }
     }
+
+    // triggers {
+    //     cron '0 0 11 * * SAT' // Run every Saturday at 11:00 AM
+    // }
 }
--- email.py
+++ email.py
@@ -1,34 +1,43 @@
 import smtplib
-from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-def send_email(recipients, sender, subject, message, smtp_server, smtp_port, smtp_username, smtp_password):
-    msg = MIMEMultipart()
-    msg['From'] = sender
-    msg['To'] = ', '.join(recipients)
-    msg['Subject'] = subject
 
-    body = message
-    msg.attach(MIMEText(body, 'plain'))
+def send_email(recipients,
+               sender,
+               message,
+               server='smtp.office365.com',
+               port=587,
+               username='',
+               password=''):
+  # Create a multipart message
+  msg = MIMEMultipart()
+  msg['From'] = sender
+  msg['To'] = ", ".join(recipients)
+  msg['Subject'] = "Weekly Maintenance Notification"
 
-    try:
-        with smtplib.SMTP(smtp_server, smtp_port) as server:
-            server.starttls()
-            server.login(smtp_username, smtp_password)
-            server.sendmail(sender, recipients, msg.as_string())
-        print("Email sent successfully!")
-    except Exception as e:
-        print(f"Error sending email: {e}")
+  # Attach the message to the MIMEMultipart object
+  msg.attach(MIMEText(message, 'plain'))
 
-if __name__ == "__main__":
-    # Example usage
-    recipients = ["John.smith@test.com", "rodger.more@test.com"]
-    sender = "no-reply-sre@test.com"
-    subject = "Planned Maintenance Notification"
-    message = "Service XYZ has planned maintenance on Saturday from 14:00 till 17:00 CET"
-    smtp_server = "your_smtp_server"
-    smtp_port = 587  # Update with your SMTP server port
-    smtp_username = "your_smtp_username"
-    smtp_password = "your_smtp_password"
+  try:
+    # Set up the SMTP server
+    with smtplib.SMTP(server, port) as server:
+      server.ehlo()
+      server.starttls()
+      server.ehlo()
+      server.login(username, password)
+      # Send the message
+      server.sendmail(sender, recipients, msg.as_string())
+      print("Email sent successfully!")
+  except Exception as e:
+    print(f"Failed to send email: {e}")
 
-    send_email(recipients, sender, subject, message, smtp_server, smtp_port, smtp_username, smtp_password)
+
+# Example usage
+recipients = ['sawhil95@gmail.com', 'rajshri0999@gmail.com', 'krishna4537@gmail.com']
+sender = 'krishna.d190798@gmail.com'
+message = 'Service XYZ has planned maintenance on Saturday from 14:00 till 17:00 CET'
+username = 'krishna.d190798@gmail.com'  # Replace with your username
+password = 'zxri sglm smlm piyc'  # Replace with your password
+
+send_email(recipients, sender, message, username=username, password=password)
